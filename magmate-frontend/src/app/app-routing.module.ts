dddf4416adc4f2e06c34164deec6b0a017449875
somi2306 <zineb.baqili@enim.ac.ma>
2025-04-15 23:21:51 +0100

    2nd commit for feature/home

--- magmate-frontend/src/app/app-routing.module.ts
+++ magmate-frontend/src/app/app-routing.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 
-const routes: Routes = [];
-=======
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // autres routes...
 ];
->>>>>>> feature/home
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
+
 export class AppRoutingModule { }
-=======
-export class AppRoutingModule { }
->>>>>>> feature/home
+
